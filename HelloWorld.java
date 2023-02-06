5645409796d114ddf4a94c91fcb741c6d55df4ec
Abigail Graber <agraber0@gmail.com>
2023-02-05 19:48:07 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,9 @@
 public class HelloWorld
-   {
+{
    public static void main(String[] args)
-      {
-         System.out.print("Hello world!");
-      }
+   {
+      System.out.println("Hello world!");
+      
+      System.out.println("Program modified, but able to revert to original using GitHub Desktop.");
    }
+}
